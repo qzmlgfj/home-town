369d9c390224474217cbae7c00f69eca502c93be
qzmlgfj <qzmlgfj@gmail.com>
2021-07-22 11:30:20 +0800

    变更后台页面标题

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
             <i v-if="!collapse" class="el-icon-s-fold"></i>
             <i v-else class="el-icon-s-unfold"></i>
         </div>
-        <div class="logo">后台管理系统</div>
+        <div class="logo">后台管理</div>
         <div class="header-right">
             <div class="header-user-con">
 <!--                &lt;!&ndash; 消息中心 &ndash;&gt;-->
